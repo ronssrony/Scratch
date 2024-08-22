4ba588dcaf99d25783f8dec1b38c1a701a60289c
ronssrony <ronssrony@gmail.com>
2024-08-22 22:06:17 +0600

    initial setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express =require('express') ; 
+const app = express() ; 
+const path = require('path') ; 
+
+
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true})) ; 
+app.use(express.static(path.join(__dirname , 'public'))); 
+app.set ('view engine' , 'ejs') ; 
+
+app.get('/',function(req, res){
+    res.render("index"); 
+})
+
+app.listen(3000,function(){
+    console.log('The server is running on port 3000')
+})
